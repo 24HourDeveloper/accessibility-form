51d2c9693d5c79c05288753566f2e0ffcdfb34a0
24hourdeveloper <mikeitt009@gmail.com>
2020-06-27 11:18:36 -0400

    first commit with form and no styles

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Student Form</h1>
+      <form>
+          <div>
+            <input type="text" placeholder="Enter First Name..."/>
+          </div>
+          <div>
+            <input type="text" placeholder="Enter Last Name..."/>
+          </div>
+          <div>
+            <h3>Sex</h3>
+            <input type="radio" name="gender" value="male"/>Male
+            <input type="radio" name="gender" value="female"/>Female
+          </div>
+          <div>
+          <h3>Hours</h3>
+            <select>
+              <option>Morning</option>
+              <option>Afternoon</option>
+              <option>Evening</option>
+            </select>
+          </div>
+          <div>
+            <h3>Courses</h3>
+            <input type="checkbox" name="course" value="java"/>Java
+            <input type="checkbox" name="course" value="javascript"/>JavaScript
+            <input type="checkbox" name="course" value="c++"/>C++
+          </div>
+          <input type="submit"/>
+      </form>
     </div>
   );
 }
