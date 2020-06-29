c537a87cdca449eb1d3e195ca60cbe0272798b38
24hourdeveloper <mikeitt009@gmail.com>
2020-06-29 08:33:04 -0400

    fixed label errors, contrast errors, empty button and fieldset

--- src/App.js
+++ src/App.js
@@ -4,36 +4,45 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <form style={styles.formStyles} className="shadow">
-          <h1 style={{marginTop:0,color:'#ccc', marginBottom:10}}>Student Form</h1>
-          <div >
-            <input type="text" placeholder="Enter First Name..." style={styles.inputStyles}/>
-          </div>
-          <div>
-            <input type="text" placeholder="Enter Last Name..." style={styles.inputStyles}/>
-          </div>
-          <div>
-            <h3 style={styles.title}>SEX</h3>
-            <input type="radio" name="gender" value="male"/>Male
-            <input type="radio" name="gender" value="female"/>Female
-          </div>
+      <div style={styles.formStyles} className="shadow">
+          <h1 style={{marginTop:0,color:'#595959', marginBottom:10}}>Student Form</h1>
+          <label htmlFor="fname" style={{display:'flex', flexDirection:'row'}}>
+            <span style={{flexGrow:1, alignSelf:'center'}}>First Name:</span>
+            <input id="fname" type="text" placeholder="Enter First Name..." style={styles.inputStyles}/>
+          </label>
+          <label htmlFor="lname" style={{display:'flex', flexDirection:'row'}}>
+            <span style={{flexGrow:1, alignSelf:'center'}}>Last Name:</span>
+            <input id="lname" type="text" placeholder="Enter Last Name..." style={styles.inputStyles}/>
+          </label>
+          <fieldset>
+            <legend style={styles.title}>SEX</legend>
+            <label htmlFor="man">
+              <input id="man" type="radio" name="gender" value="male"/>Male
+            </label>
+            <input type="radio" name="gender" value="female" aria-label="female"/>Female
+          </fieldset>
           <div>
           <h3 style={styles.title}>HOURS</h3>
-            <select>
+            <select aria-label="select hours">
               <option>Morning</option>
               <option>Afternoon</option>
               <option>Evening</option>
             </select>
           </div>
-          <div>
-            <h3 style={styles.title}>COURSES</h3>
-            <input type="checkbox" name="course" value="java"/>Java
-            <input type="checkbox" name="course" value="javascript"/>JavaScript
-            <input type="checkbox" name="course" value="c++"/>C++
-          </div>
-          <input type="submit" style={styles.btn} className='shadow-btn'/>
-          {/* <button style={styles.btn} className='shadow-btn' onClick={(e) => e.preventDefault()}>SUBMIT</button> */}
-      </form>
+          <fieldset>
+            <legend style={styles.title}>COURSES</legend>
+            <label htmlFor="java">
+              <input id="java" type="checkbox" name="course" value="java"/>Java
+            </label>
+            <label htmlFor="javascript">
+              <input id="javascript" type="checkbox" name="course" value="javascript"/>JavaScript
+            </label>
+            <label htmlFor="c++">
+              <input id="c++" type="checkbox" name="course" value="c++"/>C++
+            </label>
+          </fieldset>
+          <button style={styles.btn} className='shadow-btn'>SUBMIT</button>
+      </div>
     </div>
   );
 }
@@ -46,13 +55,13 @@ const styles = {
     borderWidth:1,
     borderColor:'#ccc',
     borderStyle:'solid',
-    width:'100%'
+    flexGrow:2
   },
   formStyles:{
     width:'30%',
     borderWidth:1,
     borderRadius:10,
-    borderColor:"#ff6257",
+    borderColor:"#B30C00",
     borderStyle:'solid',
     marginLeft:'auto',
     marginRight:'auto',
@@ -60,15 +69,15 @@ const styles = {
     padding:20,
   },
   title:{
-    color:'#ff6257',
+    color:'#B30C00',
     marginBottom:10,
     marginTop:0
   },
   btn:{
-    backgroundColor:'#ff6257',
+    backgroundColor:'#B30C00',
     color:'#fff',
     borderWidth:1,
-    borderColor:"#ff6257",
+    borderColor:"#B30C00",
     borderRadius:5,
     borderStyle:'solid',
     padding:10,
